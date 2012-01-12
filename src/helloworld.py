4f483998a39fb6bad8ba5006157d27e4c6fe5413
mwerlen <maxime@werlen.fr>
2012-01-12 15:55:56 +0100

    Super changement inutile pour tester

--- src/helloworld.py
+++ src/helloworld.py
@@ -7,7 +7,7 @@ class MainPage(webapp.RequestHandler):
     
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write('Hello, webapp World!')
+        self.response.out.write('Hello World!')
 
 
 application = webapp.WSGIApplication([('/', MainPage)], debug=True)
